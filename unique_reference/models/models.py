c4959c09548196483a468d5ee739349358459c1a
Victor Manuel <victor.alonso@xmarts.com>
2018-12-27 14:23:29 -0600

    cambio para referencia interna

--- unique_reference/models/models.py
+++ unique_reference/models/models.py
@@ -6,6 +6,7 @@ from odoo import models, fields, api
 class ResPartner(models.Model):
 	_inherit = 'res.partner'
 	ref = fields.Char(string='Referencia interna')
+	
 
 	@api.model
 	def create(self, vals):
@@ -13,29 +14,25 @@ class ResPartner(models.Model):
 		sql = "select ref from res_partner where ref != ''"
 		cr.execute(sql)
 		tabla = cr.fetchall()
-		maxc=5000
+	
 		maxp=0
 		for t in tabla:
 			try:
-				if int(t[0])>5000:
-					if int(t[0])>maxc:
-						maxc=int(t[0])
-			except ValueError:
-				print("")
-			try:
-				if int(t[0])<5000:
+				if int(t[0])>0:
 					if int(t[0])>maxp:
 						maxp=int(t[0])
 			except ValueError:
 				print("")
+		
 
 
 		if vals.get('supplier') == False:
-			vals['ref'] = maxc+1
+			vals['ref'] = maxp+1
 			
 		else:
 			vals['ref'] = maxp+1
-
+			
+		
 		return super(ResPartner, self).create(vals)
 
 class ProducProduct(models.Model):
@@ -43,7 +40,7 @@ class ProducProduct(models.Model):
 	_sql_constraints = [(
 		'default_code_unique',
 		'unique(default_code)',
-		'la referencia interna del producto ya existe, favor de modificarla'
+		'La referencia interna del producto ya existe, favor de modificarla'
 		)]
 	
 class ProTemplate(models.Model):
